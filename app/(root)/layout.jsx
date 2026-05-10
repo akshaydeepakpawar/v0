50f60d15fbfe15ac5d696327575b20bf9e398861
akshay pawar <ap3690700@gmail.com>
2026-05-10 23:38:49 +0530

    feat: implement layout and page structure with Clerk authentication

---
+++ app/(root)/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout = async ({ children }) => {
+  return (
+    <main className="flex flex-col min-h-screen relative overflow-x-hidden">
+      <div className="fixed inset-0 -z-10 h-full w-full bg-background dark:bg-[radial-gradient(#393e4a_1px,transparent_1px)] bg-[radial-gradient(#dadde2_1px,transparent_1px)] bg-size-[16px_16px]" />
+      <div className="flex-1 w-full mt-20">{children}</div>
+    </main>
+  );
+};
+
+export default Layout;
