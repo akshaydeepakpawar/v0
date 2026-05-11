3bebff660770bbdabe1e2401bf0dd47834855a2c
akshay pawar <ap3690700@gmail.com>
2026-05-11 23:57:37 +0530

    feat: implement user onboarding functionality and update Prisma schema

---
+++ modules/auth/actions/index.js
@@ -0,0 +1,77 @@
+"use server";
+
+import db from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+
+export const onBoardUser = async () => {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      return {
+        success: false,
+        error: "No authenticated user found",
+      };
+    }
+    const { id, firstName, lastName, imageUrl, emailAddresses } = user;
+
+    const newUser = await db.user.upsert({
+      where: {
+        clerkId: id,
+      },
+      update: {
+        name:
+          firstName && lastName
+            ? `${firstName} ${lastName}`
+            : firstName || lastName || null,
+        image: imageUrl || null,
+        email: emailAddresses[0]?.emailAddresses || "",
+      },
+      create: {
+        clerkId: id,
+        name:
+          firstName && lastName
+            ? `${firstName} ${lastName}`
+            : firstName || lastName || null,
+        image: imageUrl || null,
+        email: emailAddresses[0]?.emailAddresses || "",
+      },
+    });
+    return {
+      success: true,
+      user: newUser,
+      messsage: "User onboarding user:",
+    };
+  } catch (error) {
+    console.error("Error onboarding user:", error);
+    return {
+      success: false,
+      error: "Failed to onboard user",
+    };
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return null;
+    }
+    const dbUser = await db.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        image: true,
+        clerkId: true,
+      },
+    });
+    return dbUser;
+  } catch (error) {
+    console.error("Erorr fetching current user:", error);
+    return null;
+  }
+};
